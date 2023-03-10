e4e4c9ac427a9308535a555e47f45b5143a2da4c
Raynier <manfiresssj@gmail.com>
2023-03-10 12:59:25 -0400

    añadiendo nuevas soluciones a los ejercicios

--- Number 1.js
+++ Number 1.js
@@ -1,5 +1,6 @@
 // Programa una función que cuente el número de caracteres de una cadena de texto, pe. miFuncion("Hola Mundo") devolverá 10.
 
+// Solucion creada por laindomJS
 
 function countLetters(string) {
   
@@ -17,3 +18,15 @@ function countLetters(string) {
   return result
 }
 countLetters('Hello World');
+
+// Solucion creada por Raynier95
+
+const contadorPalabras=(texto)=>{
+  if(!texto) return console.warn('No es un texto')
+  let contadorCaracteres
+  for(let i=1;i<=texto.length;i++){
+    contadorCaracteres = Math.max(i)
+  }
+   console.log(`La palabra ${texto} tiene ${contadorCaracteres} letras`)
+}
+contadorPalabras('agua miel')
--- Number 3.js
+++ Number 3.js
@@ -2,6 +2,8 @@
 “<n> entre <m> da un cociente <c> y un resto <r>”, donde
 <n> y <m> son los números introducidos por el usuario, y <c> y <r> son el cociente y el resto de la división entera respectivamente. */
 
+// Solucion creada por laindomJS
+
 function showDivision(n1, n2) {
   if (typeof n1 !== 'number' || typeof n2 !== 'number') {
     return 'One parameter is wrong';
@@ -15,3 +17,14 @@ function showDivision(n1, n2) {
   return `${n1} entre ${n2} da un cociente de ${result} y un resto de ${module}`;
 }
 showDivision(10,5);
+
+// Solucion creada por Raynier95
+
+const numDivididos=(num1=undefined,num2=undefined)=>{
+  if(num1 === undefined || num2 === undefined) return console.warn(`Esta indefinido`);
+  if(typeof num1 !== 'number' || typeof num2 !== 'number') return console.warn(`No es un numero`);
+  let cociente = num1 / num2
+  let residuo = num1 % num2
+  console.log(`El cociente entre ${num1} y ${num2} es ${cociente} mientras que su residuo es ${residuo}`)
+}
+numDivididos(7,4)
