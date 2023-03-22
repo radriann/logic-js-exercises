496d7beacbbffb26d92eb74a1575670d498481c6
laindomJS <adrijar21@gmail.com>
2023-03-22 19:03:56 -0400

    feat: corrected exercise number 1

--- Number 1.js
+++ Number 1.js
@@ -1,30 +1,23 @@
 // Programa una función que cuente el número de caracteres de una cadena de texto, pe. miFuncion("Hola Mundo") devolverá 10.
 
 // Solucion creada por laindomJS
-
 function countLetters(string) {
-  
-  if (typeof string !== 'string') return 'This is no a string!';
-  if (string.length === 0) return 'The string is empty';
-  
+  if (!string || typeof string !== 'string') return 'Provide a valid string';
+
   let result;
-  let spaces = string.includes(' ');
   
-  for (let i = 0; i < string.length; i++) {  
-    spaces 
-      ? result = i 
-      : result = i + 1  
+  for (let i = 1; i < string.length; i++) {  
+    result = i   
   } 
   return result
 }
 countLetters('Hello World');
 
 // Solucion creada por Raynier95
-
-const contadorPalabras=(texto)=>{
-  if(!texto) return console.warn('No es un texto')
+const contadorPalabras = (texto) => {
+  if (!texto) return console.warn('No es un texto')
   let contadorCaracteres
-  for(let i=1;i<=texto.length;i++){
+  for (let i = 1; i <= texto.length; i++) {
     contadorCaracteres = Math.max(i)
   }
    console.log(`La palabra ${texto} tiene ${contadorCaracteres} letras`)
